6e4d871f6eac97c9fa6ee3851d0dd4d6118eccf6
Hrishikesh Mahajan <hrishikesh.22110292@viit.ac.in>
2023-04-10 13:11:23 +0530

    2023-04-18 13:11:23

---
+++ main.py
@@ -0,0 +1,10 @@
+import pymongo
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["RecipeSuggestionSystem"]
+    collection = db["Recipes"]
+    documents = collection.find({})
+    print(client)
+    for item in documents:
+        print(item)
