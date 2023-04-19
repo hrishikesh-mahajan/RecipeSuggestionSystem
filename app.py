3b1b8649431b161fe2976a626a15d1d20e991a2d
Hrishikesh Mahajan <hrishikesh.22110292@viit.ac.in>
2023-04-19 17:34:10 +0530

    2023-04-19 17:34:10

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from bson.objectid import ObjectId
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/RecipeSuggestionSystem"
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def home():
+    # documentsCount = mongo.db.Recipes.count_documents({})
+    documents = mongo.db.Recipes.find({}).limit(100)
+    return render_template("index.html", documents=documents)
+
+
+@app.route("/<_id>")
+def recipe(_id):
+    recipe = mongo.db.Recipes.find_one({"_id": ObjectId(_id)})
+    return render_template("recipe.html", recipe=recipe, _id=_id)
+
 
 if __name__ == "__main__":
-    from .views import views
-
-    app.register_blueprint(views)
-    app.run(Debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
