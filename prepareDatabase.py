a3ce0d476d9ca072bfce182af14ffc37bfbdc5a4
Hrishikesh Mahajan <hrishikesh.22110292@viit.ac.in>
2023-04-26 17:13:56 +0530

    2023-04-26 17:13:56

---
+++ prepareDatabase.py
@@ -0,0 +1,20 @@
+import json
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client["RecipeSuggestionSystem"]
+collection = db["Recipes"]
+
+for idx, document in enumerate(collection.find()):
+    print(idx, document["_id"])
+    collection.update_one({"_id": document["_id"]}, {
+        "$set": {
+            "ingredients":
+            json.loads(json.dumps(eval(str(document["ingredients"])))),
+            "steps":
+            json.loads(json.dumps(eval(str(document["steps"])))),
+            "tags":
+            json.loads(json.dumps(eval(str(document["tags"]))))
+        }
+    })
